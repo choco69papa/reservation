881d387d24e30c55be1ea46fe7d50af79c45a44e
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-01-23 08:54:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ $(function () {
         })
         names = names.slice(0, -1);
 
-        var msg = `希望日：${date}\n時間：${number\nメニュー：${names}`;
+        var msg = `希望日：${date}\n時間：${number}\nメニュー：${names}`;
         sendText(msg);
 
         return false;
