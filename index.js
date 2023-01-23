ca898108e5f8c51db80377eb769dfc05a1b7d359
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-01-23 10:50:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,18 @@ $(function () {
     });
     
 
-    
+        // 参加人数分の氏名欄を生成
+    $('#form-numbes').click(function () {
+        $('#form-name').empty();
+        var num = $('input[name="numbes"]:checked').val();
+    });
     
 
     // 送信
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
         var number = $('input[name="number"]:checked').val();
-        var number = $('input[name="numbes"]:checked').val();
+        var numbes = $('input[name="numbes"]:checked').val();
 
 
         var msg = `希望日：${date}\n人数：${number}\n氏名：${numbes}`;
