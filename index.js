de2ac0d005f0cfbb41a4e00e867a5dd0413dc4cd
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-01-24 08:23:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,15 @@ $(function () {
     });
 
     // 参加人数分の氏名欄を生成
-$('#number').change(function() {
-    var r = $('option:selected').val();
-    
-    console.log(r);
-})
-
-$(function(){
-$('#number').change(function() {
-    var r = $('option:selected').val();
-    
-    console.log(r);
-})
-});
+    $('#form-number').click(function () {
+        $('#form-name').empty();
+        var num = $('input[name="number"]:checked').val();
+        for (i = 0; i < num; i++) {
+            $('#form-name').append(
+                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
+            );
+        }
+    });
 
     // 送信
     $('form').submit(function () {
