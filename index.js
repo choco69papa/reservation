59f40fe3a60303cfe1324da14c5d037787ccb8f8
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-01-24 18:30:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ $(function () {
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
         var number = $('input[name="number"]:checked').val();
-        var names = '';
-        $('#form-name').children().each(function (i, elm) {
-            names += $(elm).val() + '、';
-        })
+        var names = $('input[name="names"]:checked').val();
         names = names.slice(0, -1);
         
         var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
