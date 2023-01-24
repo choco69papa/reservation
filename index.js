7b1947a964be265b504d7ac812feff54afefd8a2
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-01-24 13:43:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ $(function () {
         $('#form-name').empty();
         var num = $('input[name="number"]:checked').val();
         for (i = 0; i < num; i++) {
-            $('#form-name').append(
-                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
-            );
         }
     });
 
