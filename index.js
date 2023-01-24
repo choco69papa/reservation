496323f4f7c928a96012a0281cbf3cdc6693fd5f
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-01-25 01:54:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ $(function () {
             minDate: 0
         });
     });
-    $(function(){ $(“#datepicker”).datepicker(); $(“#datepicker”).datepicker(‘option’,’beforeShowDay’,function(date){ var ret = [(date.getDay() != 1 && date.getDay() != 6)]; return ret; });});
 
     // 参加人数分の氏名欄を生成
     $('#form-number').click(function () {
