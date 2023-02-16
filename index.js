e83b3c5eb7ee7cb9a3f0203da4b419d48294317c
choco69papa <122781148+choco69papa@users.noreply.github.com>
2023-02-16 19:18:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ $(function () {
         var names = $('select[name="names"]').val();
         var inquiries = $('textarea[name="inquiries"]').val();
 	    
-        var msg = `〓ご予約内容〓\n\n希望日：${date}\n\n時間：${minute}\n\nメニュー：${names}\f問い合わせ内容：${inquiries}`;
+        var msg = `〓ご予約内容〓\n\n希望日：<br>${date}\n\n時間：${minute}\n\nメニュー：<br>${names}\n\n問い合わせ内容：<br>${inquiries}`;
         sendText(msg);
 
         return false;
